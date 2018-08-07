9b86eccbcec68767c52d3ed643b92e8e037190af
Yun Liu <vagrantlyun@gmail.com>
2018-08-06 23:04:33 -0500

    Update segment-graph.h

--- include/caffe/segment/segment-graph.h
+++ include/caffe/segment/segment-graph.h
@@ -22,6 +22,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 #include <algorithm>
 #include <cstdlib>
 #include <math.h>
+#include "image.h"
 #include "disjoint-set.h"
 #include "caffe/blob.hpp"
 
